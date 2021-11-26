7f619c6660f43f5bd8045c43dc483485bdd22c4b
OwaisKhan <khanow@mail.gvsu.edu>
2021-11-26 07:38:01 +0000

    Startup file: importing data and storing them in variables.

--- Startup.R
+++ Startup.R
@@ -1,24 +1,31 @@
 #---------------Libraries----------------
 
 install.packages("textcat")
+install.packages("tictoc")
+
+install.packages("Shiny")
 
 library(tidyr)
 library(dplyr)
 library(tidyverse)
 library(textcat)
+library(data.table)
+library(shiny)
 
 #-------------Implementation-----------
 
-    india_wants_oxygen <- read_csv("IndiaWantsOxygen.csv")
+india_wants_oxygen <- read_csv("IndiaWantsOxygen.csv")
 day_wise  <- read_csv("day_wise.csv")
 state_wise_testing_details <- read_csv("StatewiseTestingDetails.csv")
 covid_19_clean_complete <- read_csv("covid_19_clean_complete.csv")
-covid_19_clean_complete.csvView(day_wise)
+covid19_india <- read_csv("covid_19_india.csv")
 
 #Get the maximum positive cases of each state
 state_wise_testing_details %>% group_by(State) %>%  slice(which.max(Positive)) %>% View()
 
+state_wise_testing_details %>% View()
 
+jan1st  <- 
 
 textcat("ஏழைகள் அவெஞ்சர்")
 
