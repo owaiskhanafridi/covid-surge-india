39aafc70854d274b8de03865f991212bd18185d6
OwaisKhan <khanow@mail.gvsu.edu>
2021-12-09 20:26:19 +0000

    Shiny dashboard

---
+++ app.R
@@ -0,0 +1,26 @@
+#install.packages("shinydashboard")
+library(shiny)
+library(shinydashboard)
+
+ui <- dashboardPage(
+  dashboardHeader(title = "Covid Surge India"),
+  dashboardSidebar(),
+  dashboardBody(
+    box(plotOutput("correlation_plot"), width = 8),
+    box(
+      selectInput("features", "Features:",
+                  c("Sepal.Width", "Petal.Length", "Petal.Width")
+                  ), width = 4
+    )
+  )
+)
+
+server <- function(input, output) {
+output$correlation_plot <- renderPlot({
+  plot(iris$Sepal.Length, iris[[input$features]], 
+       xlab = "Sepal Length", ylab = "Feature"
+       )
+})   
+}
+
+shinyApp(ui, server)
