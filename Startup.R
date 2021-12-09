edda80ccd3e9fb9284c76ad9fbe0e3353b6dd45b
OwaisKhan <khanow@mail.gvsu.edu>
2021-12-09 20:25:37 +0000

    updated startup code

--- Startup.R
+++ Startup.R
@@ -12,7 +12,40 @@ library(textcat)
 library(data.table)
 library(shiny)
 
-#-------------Implementation-----------
+install.packages("rgeos")
+
+library(ggplot2)
+library(maptools)
+library(rgeos)
+library(ggmap)
+library(scales)
+library(RColorBrewer)
+library(sf)
+
+library(maps)
+library(ggplot2)
+
+
+
+ind <- map(database = "world", regions = "india", exact = FALSE, boundary = T)
+india <- map_data(ind , region = "india", exact = F)
+
+(ggplot(aes(x=x, y=y, fill=z), data=state_wise_testing_details) + geom_tile()) + geom_polygon(data=india, aes(x=long, y=lat, group=group), colour="black", fill="red", alpha=0)
+
+
+states_map <- map_data("state", country="india")
+fee_map <- merge(states_map, spendstate, by.x = "region", by.y = "state")
+
+
+?map_data
+
+states_map %>% View()
+
+IND<-getCountries("IND",level=2)
+
+df1 = gpd.read_file('India_Districts/c44c9b96-f570-4ee3-97f1-ebad64efa4c2202044-1-1rb4x6s.8xx6.shp')
+
+#-------------Reading Data-----------
 
 india_wants_oxygen <- read_csv("IndiaWantsOxygen.csv")
 day_wise  <- read_csv("day_wise.csv")
@@ -30,3 +63,27 @@ jan1st  <-
 textcat("ஏழைகள் அவெஞ்சர்")
 
 
+
+#-------------State Wise Positivity-----------
+
+#Data Reading
+state_wise_testing_details <- read_csv("StatewiseTestingDetails.csv")
+
+#Data Cleaning
+
+#Considering rows which have atleast one column (Positive, Negative) value available.
+#Evaluating missing value from the available value.
+#Calculating the covid positivity rate. 
+
+cleaned_data <- state_wise_testing_details %>%  
+  filter(!is.na(Positive) | !is.na(Negative)) %>% 
+  mutate(
+    Negative = ifelse(is.na(Negative), TotalSamples - Positive, Negative), 
+    Positive = ifelse(is.na(Positive), TotalSamples - Negative, Positive),
+    Positive_Rate = round((Positive/TotalSamples) * 100, 2)
+    )
+
+
+shp <- rgdal::readOGR('~/STA 518/STA518_Project/covid-surge-india/Admin2.shx')
+
+shp %>% View()
